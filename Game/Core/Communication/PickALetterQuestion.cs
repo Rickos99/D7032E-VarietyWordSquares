1907f6c805132ecd2e051c01d1e4a67166b9507c
Rickos99 <Rickos99@users.noreply.github.com>
2021-10-13 23:32:31 +0200

    Added new question types mainly for bots

---
+++ Game/Core/Communication/PickALetterQuestion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Core.Communication
+{
+    class PickALetterQuestion : IQuestion
+    {
+        public IList<Choice> Choices { get; private set; }
+
+        public bool HasChoices { get; private set; }
+
+        public string Content { get; private set; }
+
+        public PickALetterQuestion(bool isScrabbleMode)
+        {
+            Choices = null;
+            HasChoices = false;
+            Content = string.Empty;
+            if (isScrabbleMode)
+            {
+                Content += "LETTER POINTS\n"; // TODO Get from a dictionary
+            }
+            Content += "Pick a letter";
+        }
+
+        public string GetMessageString()
+        {
+            return Content;
+        }
+    }
+}
---
+++ Game/Core/Communication/PickLetterLocationQuestion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Core.Communication
+{
+    class PickLetterLocationQuestion : IQuestion
+    {
+        public IList<Choice> Choices { get; private set; }
+
+        public bool HasChoices { get; private set; }
+
+        public string Content { get; private set; }
+
+        public PickLetterLocationQuestion(char letter)
+        {
+            Choices = null;
+            HasChoices = false;
+            Content = $"Place {letter} (syntax: row column)";
+        }
+
+        public string GetMessageString()
+        {
+            return Content;
+        }
+    }
+}
