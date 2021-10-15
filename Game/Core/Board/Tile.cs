ed208bed92a053b7775495168c60c525b0012d83
Rickos99 <Rickos99@users.noreply.github.com>
2021-10-15 12:21:59 +0200

    Renamed point property

--- Game/Core/Board/Tile.cs
+++ Game/Core/Board/Tile.cs
@@ -3,12 +3,12 @@
     public class Tile
     {
         public char Letter { get; private set; }
-        public int TilePoint { get; private set; }
+        public int Points { get; private set; }
 
-        public Tile(char letter, int tilePoint)
+        public Tile(char letter, int points)
         {
             Letter = letter;
-            TilePoint = tilePoint;
+            Points = points;
         }
     }
 }
