b4fc6d0e7fa3c95afbedb13e1133386c7f281afb
Rickos99 <Rickos99@users.noreply.github.com>
2021-10-16 23:50:16 +0200

    Removed unused imports

--- Game/Core/Board/IBoard.cs
+++ Game/Core/Board/IBoard.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Game.Core.Board
+﻿namespace Game.Core.Board
 {
     public interface IBoard
     {
