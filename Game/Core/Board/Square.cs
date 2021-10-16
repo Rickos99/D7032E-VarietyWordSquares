237e8b7c7bd3adad782c518a4a8baa58191f6edc
Rickos99 <Rickos99@users.noreply.github.com>
2021-10-16 23:52:54 +0200

    Added second ctor and documentation

--- Game/Core/Board/Square.cs
+++ Game/Core/Board/Square.cs
@@ -2,10 +2,36 @@
 {
     public class Square
     {
+        /// <summary>
+        /// Indicate the scrabble point multiplier type
+        /// </summary>
         public SquareType SquareType { get; private set; }
+
+        /// <summary>
+        /// Tile placed on square
+        /// </summary>
         public Tile Tile { get; set; }
+
+        /// <summary>
+        /// Indicate if <see cref="Tile"/> is empty.
+        /// </summary>
         public bool IsEmpty { get => Tile == null; }
 
+        /// <summary>
+        /// Initialize a new instance of the <see cref="Square"/> class with a 
+        /// type and no tile.
+        /// </summary>
+        /// <param name="squareType">Indicate the scrabble point multiplier</param>
+        public Square(SquareType squareType) : this(squareType, null)
+        {
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="Square"/> class with a 
+        /// type and a tile.
+        /// </summary>
+        /// <param name="squareType">Indicate the scrabble point multiplier</param>
+        /// <param name="tile">A tile to add to square</param>
         public Square(SquareType squareType, Tile tile)
         {
             SquareType = squareType;
