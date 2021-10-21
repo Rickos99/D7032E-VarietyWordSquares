a21b13568ef3b4ec28ace775315b680c970e4973
Rickos99 <Rickos99@users.noreply.github.com>
2021-10-21 22:08:42 +0200

    Added `Settings` class

---
+++ Game/Core/Resources/Settings.cs
@@ -0,0 +1,111 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace Game.Core.Resources
+{
+    static class Settings
+    {
+        public static uint BoardRowSize
+        {
+            get => uint.Parse(GetSetting("Board.Size.Rows"));
+            set => SetSetting("Board.Size.Rows", value.ToString());
+        }
+
+        public static uint BoardColumnSize
+        {
+            get => uint.Parse(GetSetting("Board.Size.Columns"));
+            set => SetSetting("Board.Size.Columns", value.ToString());
+        }
+
+        public static uint NumberOfPlayers
+        {
+            get => uint.Parse(GetSetting("Player.Humans"));
+            set => SetSetting("Player.Humans", value.ToString());
+        }
+
+        public static uint NumberOfBots
+        {
+            get => uint.Parse(GetSetting("Player.Bots"));
+            set => SetSetting("Player.Bots", value.ToString());
+        }
+
+        public static string DictionaryFolder
+        {
+            get => GetSetting("Resources.DictionaryFolder");
+        }
+
+        public static string DictionaryFile
+        {
+            get => GetSetting("Resources.DictionaryFile");
+            set => SetSetting("Resources.DictionaryFile", value);
+        }
+
+        public static string BoardFolder
+        {
+            get => GetSetting("Resources.BoardFolder");
+        }
+
+        public static string BoardFile
+        {
+            get => GetSetting("Resources.BoardFile");
+            set => SetSetting("Resources.BoardFile", value);
+        }
+
+        public static string TileSchemaFolder
+        {
+            get => GetSetting("Resources.TileSchemaFolder");
+        }
+
+        public static string TileSchemaFile
+        {
+            get => GetSetting("Resources.TileSchemaFile");
+            set => SetSetting("Resources.TileSchemaFile", value);
+        }
+
+
+
+        public static string[] GetAvaliableDictionaryFiles()
+        {
+            return GetFileNamesInFolder(DictionaryFolder);
+        }
+
+        public static string[] GetAvaliableBoardFiles()
+        {
+            return GetFileNamesInFolder(BoardFolder);
+        }
+
+        public static string[] GetAvaliableTileSchemaFiles()
+        {
+            return GetFileNamesInFolder(TileSchemaFolder);
+        }
+
+        private static string[] GetFileNamesInFolder(string directory)
+        {
+            return Directory.GetFiles(directory)
+                .Select((f) => Path.GetFileName(f))
+                .ToArray();
+        }
+
+        private static string GetSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        private static void SetSetting(string key, string value)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            if (settings[key] == null)
+            {
+                settings.Add(key, value);
+            }
+            else
+            {
+                settings[key].Value = value;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        }
+    }
+}
