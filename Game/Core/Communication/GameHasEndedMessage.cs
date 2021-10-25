a287212e43e71c33a68026ab054a2b2f42eac631
Rickos99 <Rickos99@users.noreply.github.com>
2021-10-25 10:32:21 +0200

    Create GameHasEndedMessage.cs

---
+++ Game/Core/Communication/GameHasEndedMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.Core.Communication
+{
+    /// <summary>
+    /// A message used when a game has ended and to tell a network client to disconnect.
+    /// </summary>
+    class GameHasEndedMessage : IMessage
+    {
+        public string Content => "Game has been ended by host. \n\n Thanks for playing!";
+
+        public string GetMessageString()
+        {
+            return Content;
+        }
+    }
+}
