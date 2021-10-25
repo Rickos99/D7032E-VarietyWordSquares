e3024ba5628b98b7004369fd924ae810f7841c4e
Rickos99 <Rickos99@users.noreply.github.com>
2021-10-25 12:49:07 +0200

    Replaced the old `GameModeBase`

---
+++ Game/Core/GameModes/Rules/IGameRules.cs
@@ -0,0 +1,23 @@
+﻿using Game.Core.Board;
+
+namespace Game.Core.GameModes.Rules
+{
+    /// <summary>
+    /// Used to define a set of rules in a <see cref="AbstractGameMode"/>
+    /// </summary>
+    interface IGameRules
+    {
+        /// <summary>
+        /// Calculate a score for the specified board.
+        /// </summary>
+        /// <param name="board">Bord to calculate a score for.</param>
+        /// <returns>A score</returns>
+        int CalculateScore(StandardBoard board);
+
+        /// <summary>
+        /// Check whether the specified board fulfill the gameover condition.
+        /// </summary>
+        /// <returns><c>true</c> if the board fulfill the gameover condition; Otherwise <c>false</c>. </returns>
+        bool BoardHasReachedGameOver();
+    }
+}
